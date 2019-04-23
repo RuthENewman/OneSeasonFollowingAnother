fcb9e004f73a2f5f951a7bbe458965e98b607cd7
Ruth Newman <ruthnewman@ruths-air.home>
2019-04-23 23:57:20 +0100

    fully working site and with media query

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      document.getElementById('geo').style.display = 'none';
+      getToday(lat, long);
+    });
+  }
+});
+
 const baseURL = "https://api.sunrise-sunset.org/json?";
 const wrapperDiv = document.querySelector('.wrapper');
 const tomorrowButton = document.querySelector('.tomorrow');
+const todayButtonDiv = document.querySelector('.todayButton');
 
 function getToday(latitude, longitude) {
   fetch(`${baseURL}lat=${latitude}&lng=${longitude}&date=today`)
     .then(response => response.json())
     .then(data => {
-      console.log(data.results)
-      let sunrise = data.results.sunrise;
-      let sunset = data.results.sunset;
+      let sunrise = data.results.sunrise.slice(0,4);
+      let sunset = data.results.sunset.slice(0,4);
       wrapperDiv.innerHTML = `<div class="today">
-        <h3>Sunrise time today: ${sunrise}</h3>
-        <h3>Sunset time today: ${sunset}</h3>
+        <h3>Sunrise time today: <span class="time">${sunrise} AM</span></h3>
+        <h3>Sunset time today: <span class="time">${sunset} PM</span></h3>
       </div>`;
+      todayButtonDiv.style.display = 'none';
+      tomorrowButton.style.display = 'block';
     })
 }
 
@@ -20,17 +36,29 @@ function getTomorrow(latitude, longitude) {
   fetch(`${baseURL}lat=${latitude}&lng=${longitude}&date=tomorrow`)
     .then(response => response.json())
     .then(data => {
-      console.log(data.results)
-      let sunrise = data.results.sunrise;
-      let sunset = data.results.sunset;
+      let sunrise = data.results.sunrise.slice(0,4);
+      let sunset = data.results.sunset.slice(0,4);
       wrapperDiv.innerHTML = `<div class="tomorrowDetails">
-        <h3>Sunrise time tomorow: ${sunrise}</h3>
-        <h3>Sunset time tomorrow: ${sunset}</h3>
+        <h3>Sunrise time tomorow: <span class="time">${sunrise} AM</span></h3>
+        <h3>Sunset time tomorrow: <span class="time">${sunset} PM</span></h3>
       </div>`;
+      document.querySelector('.tomorrow').style.display = 'none';
+      document.querySelector('.todayButton').style.display = 'block';
     })
 }
 
-tomorrow.addEventListener('click', event => {
-  event.preventDefault();
-  // getTomorrow()
+tomorrowButton.addEventListener('click', event => {
+  navigator.geolocation.getCurrentPosition(position => {
+    long = position.coords.longitude;
+    lat = position.coords.latitude;
+    getTomorrow(lat, long);
+  });
 })
+
+todayButtonDiv.addEventListener('click', event => {
+  navigator.geolocation.getCurrentPosition(position => {
+    long = position.coords.longitude;
+    lat = position.coords.latitude;
+    getToday(lat, long);
+  });
+});
